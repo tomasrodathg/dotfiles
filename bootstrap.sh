93b6f55f77aacc6aa765225fd4ff13835c3dfd3e
tomasrodathg <tomas.roda@thehutgroup.com>
2024-02-19 19:22:06 +0000

    Updated bootstrap to include plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
 # Bring in our custom neovim config
 echo "XDG_CONFIG_HOME=$HOME" >> ~/.profile
-git clone https://github.com/tomasrodathg/neovim-config ~/.config/nvim
+git clone https://github.com/tomasrodathg/nvim-config ~/.config/nvim
 cd ~/.config/nvim
 git submodule init
 git submodule update
@@ -23,6 +23,16 @@ git submodule update
 cd /workspaces/$RepositoryName
 # poetry run pip install pynvim ipython  # Avoid if we can help it
 poetry run nvim --headless +":UpdateRemotePlugins" +"q!"
+
+# Install vim-plugged for plugins 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# create directory for plugins
+mkdir ~/.vim/plugged
+
+# Install plugins
+poetry run nvim --headless +":PlugInstall" + "q!"
 
 # Setup git completions for bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
